a0cebb00872a0c04a97c9c0f4035cfb5edaa8a49
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-18 03:45:29 -0400

    debugging

--- api/authors/update.php
+++ api/authors/update.php
@@ -3,8 +3,7 @@
     header('Access-Control-Allow-Origin: *');
     header('Content-Type: application/json');
     header('Access-Control-Allow-Methods: DELETE');
-    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type,
-    Access-Control-Allow-Methods, Authorization, X-Requested-With');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once '../../config/Database.php';
     include_once '../../models/Author.php';
@@ -24,13 +23,13 @@
 
 
     //dalete post
-    if($author->delete()){
+    if($author->update()){
         echo json_encode(
-            array('message' => 'Author Deleted')
+            array('message' => 'Author Updated')
         );
     } else {
         echo json_encode(
-            array('message' => 'Author Not Deleted')    
+            array('message' => 'Author Not Updated')    
         );
     }
 
