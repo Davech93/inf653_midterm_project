ab2259447b08b636ac422d4fbd5b3d562320c886
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-18 16:37:31 -0400

    syntax error

---
+++ api/quotes/create.php
@@ -0,0 +1,35 @@
+<?php
+    //headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+    header('Access-Control-Allow-Methods: POST');
+    header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Category.php';
+
+    //Instantiate DB & Connect
+    $database = new Database();
+    $db = $database ->connect();
+
+    //Instantiate Author Object
+    $category = new Category($db);
+
+    // get raw posted data
+    $data = json_decode(file_get_contents("php://input"));
+
+    $category->category = $data->category;
+    $category->id = $data->id;
+
+    //create post
+    if($category->create()){
+        echo json_encode(
+            array('message' => 'Category Created')
+        );
+    } else {
+        echo json_encode(
+            array('message' => 'Category Not Created')    
+        );
+    }
+
+    ?>
---
+++ api/quotes/read.php
@@ -0,0 +1,56 @@
+<?php
+    //headers
+    header('Access-Control-Allow-Origin: *');
+    header('Content-Type: application/json');
+
+    include_once '../../config/Database.php';
+    include_once '../../models/Category.php';
+
+    //Instantiate DB & Connect
+    $database = new Database();
+    try {
+        // Connect to the database
+        $db = $database->connect();
+    
+    } catch (PDOException $e) {
+        // Log the error message to the browser console
+        echo '<script>console.error(' . json_encode($e->getMessage()) . ');</script>';
+    }
+
+    //Instantiate Category Object
+    $category = new Category($db);
+
+    //Author Query
+    $result = $category->read();
+
+    //Get row count
+    $num = $result->rowCount();
+
+    //Check if any category
+    if($num > 0){
+        //Author Array
+        $category_arr = array();
+        $category_arr['data'] = array();
+
+        while($row = $result->fetch(PDO::FETCH_ASSOC)) {
+            extract($row);
+
+            $category_item = array(
+                'category' => $category,
+                'id' => $id
+            );
+
+            //push to "data
+            array_push($category_arr['data'], $category_item);
+        }
+
+        //turn to json & output
+        echo json_encode($category_arr);
+
+    } else {
+        //No Authors
+        echo json_encode(
+            array('message' => 'No categories found')
+        );
+    }
+    ?>
