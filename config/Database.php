f1950e1fb67cd1e21e57abb639ded82c56063389
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-19 17:20:28 -0400

    debug

--- config/Database.php
+++ config/Database.php
@@ -21,11 +21,10 @@
             if ($this->conn){
                 return $this->conn;
             } else{
-                
+                $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
 
                 try {
                     //log all values 
-                    $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname};";
                     $this->conn = new PDO($dsn, $this->username, $this->password);
                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     return $this->conn;
