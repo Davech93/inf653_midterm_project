d3c3389013d88abde7a0a930c288b103178abc41
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-20 23:30:24 -0400

    debug

--- models/Quote.php
+++ models/Quote.php
@@ -51,12 +51,7 @@
     //Get single author
 
     public function check_id(){
-        $query= 'SELECT
-        q.id
-        FROM
-        ' . $this->table . 'q
-        WHERE q.id = ?
-        LIMIT 1 OFFSET 0';
+        $query = 'SELECT q.id FROM ' . $this->table . 'q WHERE q.id = ? LIMIT 1 OFFSET 0';
 
         $stmt = $this->conn->prepare($query);
         $stmt->bindParam(1, $this->id);
