c987c44afcb6c887b2b697046b4a776e90f2c23e
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-20 20:51:45 -0400

    debug

--- api/quotes/read.php
+++ api/quotes/read.php
@@ -8,14 +8,9 @@
 
     //Instantiate DB & Connect
     $database = new Database();
-    try {
-        // Connect to the database
-        $db = $database->connect();
+    // Connect to the database
+    $db = $database->connect();
     
-    } catch (PDOException $e) {
-        // Log the error message to the browser console
-        echo '<script>console.error(' . json_encode($e->getMessage()) . ');</script>';
-    }
 
     //Instantiate Category Object
     $quote = new Quote($db);
