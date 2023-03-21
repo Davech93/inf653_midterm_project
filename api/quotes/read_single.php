7a4fb896a62ee08322ddd19a76eb0a94328b45c7
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-21 00:14:15 -0400

    debig

--- api/quotes/read_single.php
+++ api/quotes/read_single.php
@@ -18,6 +18,7 @@
     $quote->id = isset($_GET['id']) ? $_GET['id'] :die();
 
     $quote->check_id();
+    echo 'error 2' . gettype($quote->id);
     echo 'ERROR!!' . $quote->id;
     if (!$quote->id){
         $quote_arr = array(
