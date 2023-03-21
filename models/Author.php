825a1974b63ce9b6612e076a4c6f0ed560bc7728
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-21 18:11:28 -0400

    debug

--- models/Author.php
+++ models/Author.php
@@ -84,6 +84,9 @@
 
             //execute query
             if($stmt->execute()){
+                $this->id =$row['id'];
+                $check_arr = array('id' => $id, 'author' =>$author);
+                echo(json_encode($check_arr));
                 return true;
             }
             //print error if something goes wrong
