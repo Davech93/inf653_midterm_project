e2025bee7178a7b8fa4bbc7ed47d7a458e20b9d4
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-22 06:00:53 -0400

    debug

--- api/authors/create.php
+++ api/authors/create.php
@@ -20,19 +20,17 @@
     $data = json_decode(file_get_contents("php://input"));
     //print_r($data);
     
-    
-    if(isset($data->author)){
+    $author->author = $data->author;
+    //if(isset($data->author)){
      //create author
      if($author->create()){
       //  echo json_encode(array("id" => $author->id,'message' => 'Author Deleted'));
-        $author->author = $data->author;
+       
         print_r($data->author);
       //  $author->id = $id;
         echo json_encode(array( "author" => $author->author));
       } else {
         echo json_encode(array('message' => 'Author Not Created'));
     } 
-  } else {
-    echo json_encode(array('message' => 'Missing Required Parameters'));
-}
+  
     ?>
