631124c593047576eef97cef341321ae3839b6b7
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-23 01:51:28 -0400

    debug

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -7,6 +7,8 @@
 
     include_once '../../config/Database.php';
     include_once '../../models/Quote.php';
+    include_once '../../models/Categories.php';
+    include_once '../../models/Authors.php';
 
     //Instantiate DB & Connect
     $database = new Database();
@@ -14,21 +16,37 @@
 
     //Instantiate Author Object
     $quote = new Quote($db);
+    $category = new Category($db);
+    $author = new Author($db);
 
     // get raw posted data
     $data = json_decode(file_get_contents("php://input"));
 
+    $author->id = $data->author_id;
+    $category->id = $data->category_id;
     $quote->quote = $data->quote;
     //if(isset($data->quote)){
      //create quote
+     
      if($quote->create()){
       //  echo json_encode(array("id" => $quote->id,'message' => 'Author Deleted'));
-       
+        if(!$category->read_single()){
+          echo json_encode(array("category_id Not Found"));
+     
+      } else if(!$author->read_single()){
+          echo json_encode(array("author_id Not Found"));
         
-      //  $quote->id = $id;
+     } else if ($author->read_single() && $category->read_single()){}
+          
         echo json_encode(array( "id"=>$quote->id, "quote" => $quote->quote, "author_id"=> $quote->author_id, "category_id" => $quote->category_id));
       } else {
         echo json_encode(array('message' => 'Missing Required Parameters'));
-    } 
+      }
+   
+        
+      //  $quote->id = $id;
 
+       
+// seperate queries to the author or category tables WHERE id =
+// and if nothing found return that
     ?>
