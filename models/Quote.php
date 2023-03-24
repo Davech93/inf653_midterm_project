1d7bac503ea1947853fd3c3ecca82077792605aa
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-23 20:42:24 -0400

    debug

--- models/Quote.php
+++ models/Quote.php
@@ -189,7 +189,7 @@
         }
     
         public function isValid($id, $model){
-            $id = $this->id;
+            $this->id=$row['id'];
             $result = $model->read_single();
             return $result;
         }
