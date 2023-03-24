1080793aa8099b17213479413ada8f8d937c24dd
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-24 18:12:18 -0400

    debug

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -34,6 +34,12 @@
       //echo json_encode(array( "id"=>null, "quote" => null, "author_id"=> null, "category_id" => null));
      
         exit();
+  } else {
+    $result = $category->isValidCatId($category);
+    if ($result == false) {
+      echo json_encode(array("message"=>"category_id Not Found"));
+      exit();
+     }
   }
     
    if(isset($data->author_id)){
@@ -43,6 +49,12 @@
       //echo json_encode(array( "id"=>null, "quote" => null, "author_id"=> null, "category_id" => null));
      
         exit();
+   } else {
+    $result2 = $author->isValidAutId($author);
+    if ($result2 == false){
+      echo json_encode(array("message"=>"author_id Not Found"));
+      exit();
+       }
    }
 
    if(isset($data->author_id)){
@@ -59,19 +71,13 @@
    
      
      
-     $result2 = $author->isValidAutId($author);
+    
 
-     if ($result2 == false){
-      echo json_encode(array("message"=>"author_id Not Found"));
-      exit();
-       }
+     
 
-       $result = $category->isValidCatId($category);
+       
         
-      if ($result == false) {
-        echo json_encode(array("message"=>"category_id Not Found"));
-        exit();
-       }
+      
      
      if ($quote->create()){
       echo json_encode(array( "id"=>$id, "quote" => $quote->quote, "author_id"=> $quote->author_id, "category_id" => $quote->category_id));
