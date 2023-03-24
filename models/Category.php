6fa730c231c4403fe2535c1df134c6a6daa835b6
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-24 00:58:42 -0400

    debug

--- models/Category.php
+++ models/Category.php
@@ -164,7 +164,7 @@
                 echo json_encode($a);
             } else {
                 $result = $model->read_single();
-                if($model->category_id && $model->category) {
+                if($model->category_id ) {
                    return true;
                 } else {
                     return false;
