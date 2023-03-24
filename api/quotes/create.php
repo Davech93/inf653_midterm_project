1c276058494066ba8f500f6df2a1ebfb1f42e83f
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-24 00:47:32 -0400

    debug

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -23,6 +23,7 @@
     // $data = json_decode(file_get_contents("php://input"));
     //$data = json_decode(file_get_contents("php://input"));
     $data = $_REQUEST;
+    print_r($data);
 
      $author->id = $data['author_id'];
      $category->id= $data['category_id'];
