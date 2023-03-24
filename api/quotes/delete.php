e944f732720b5f42692f559a92c5affef58554f1
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-23 21:33:25 -0400

    debug

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -23,8 +23,12 @@
     $result = $quote->isValid($quote);
     if ($result == true){
       $quote->delete();
+      $obj1 = {'id' => $quote->id};
+      echo json_encode($obj1);
       echo json_encode('id: ' . $quote->id);
     } else if ($result == false){
+      $obj = {'message' => 'No Quotes Found'};
+      echo json_encode($obj);
       echo json_encode('message: ' . 'No Quotes Found');
     }
 
