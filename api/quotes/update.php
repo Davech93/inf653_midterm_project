a06e1d2eab22a5a4714ca8618209a2d4e68a49e8
DAVID HARMON <DAVECH93@GMAIL.COM>
2023-03-24 23:57:57 -0400

    debug

--- api/quotes/update.php
+++ api/quotes/update.php
@@ -38,13 +38,13 @@
         exit();
        }
        if(isset($data->author_id)){
-        $quote->$author_id = $data->author_id;
+        $quote->author_id = $data->author_id;
        } else {
         echo json_encode(array("message"=>"Missing Required Parameters"));
         exit();
        }
        if(isset($data->category->id)){
-        $quote->$category_id = $data->category_id;
+        $quote->category_id = $data->category_id;
        } else {
         echo json_encode(array("message"=>"Missing Required Parameters"));
         exit();
